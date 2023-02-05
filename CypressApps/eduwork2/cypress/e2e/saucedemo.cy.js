a5e4501e9c872c186098eb550b4944669d7a49c4
putsl5 <81682878+putsl5@users.noreply.github.com>
2023-02-06 00:23:39 +0700

    add new file 6

--- CypressApps/eduwork2/cypress/e2e/saucedemo.cy.js
+++ CypressApps/eduwork2/cypress/e2e/saucedemo.cy.js
@@ -6,9 +6,8 @@ describe('Working with inputs', () => {
         cy.url().should('include', '')
     });
 
-    
-
-    it('should try fixtures data', () => { 
+    it('should check product', () => { 
+       
         cy.fixture("customer").then(user => {
             const username = user.username
             const password = user.password
@@ -22,19 +21,43 @@ describe('Working with inputs', () => {
             cy.contains('Login').click()
 
         })
-    });
-    
-    it('should check product', () => { 
+       
         cy.fixture("produk").then(user => {
             // untuk looping
             cy.wrap(user.products).each((index)=> {
                 cy.get('.inventory_item_name').contains(index.produk)
                 cy.get('#add-to-cart-' + index.label).click()
             })
-            //cy.get('a').should('.shopping_cart_link')
         })
+        cy.get('.shopping_cart_link').click()
+        cy.get('#continue-shopping').click()
+        cy.get('#remove-sauce-labs-bike-light').click()
+        cy.get('.shopping_cart_link').click()
+        cy.get('#checkout').click()
+
+        cy.get('span').should('contain.text', 'Checkout: Your Information')
+
+        cy.get('#first-name').clear()
+        cy.get('#first-name').type('Putri')
+
+        cy.get('#last-name').clear()
+        cy.get('#last-name').type('Salsabila')
+
+        cy.get('#postal-code').clear()
+        cy.get('#postal-code').type('45363')
+       
+
+        cy.get('#continue').click()
+        cy.get('span').should('contain.text', 'Checkout: Overview')
+        cy.get('#finish').click()
+        cy.get('span').should('contain.text', 'Checkout: Complete!')
+
+        cy.get('#back-to-products').click()
+        cy.get('span').should('contain.text', 'Products')
+
+        cy.get('#react-burger-menu-btn').click()
+        cy.get('#logout_sidebar_link').click()
+
     });
-
-    
-
+   
 });
